2e857f256c34abd485fdf0abdb0ef3823362a355
jamespink <james.r.pink@gmail.com>
2017-05-22 23:21:36 +0100

    App functional just requires last tiding

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetIntentService.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetIntentService.java
@@ -22,14 +22,22 @@ import com.udacity.stockhawk.ui.MainActivity;
 
 import org.stringtemplate.v4.ST;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  * Created by jamespink on 13/05/2017.
  */
 
 public class StockWidgetIntentService extends IntentService {
 
+    private DecimalFormat dollarFormatWithPlus;
+    private DecimalFormat dollarFormat;
+
     private static final String PREFS_NAME
             = "com.udacity.stockhawk.widget.StockWidgetProvider";
+    private static final String PREF_PREFIX_KEY = "prefix_";
 
     public static final String[] STOCK_HISTORY_PROJECTION = {
             Contract.Quote.COLUMN_SYMBOL,
@@ -49,7 +57,14 @@ public class StockWidgetIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+
+        dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus.setPositivePrefix("+$");
+
+
         //Retreive widget id's
+        /*
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
                 StockWidgetProvider.class));
@@ -66,17 +81,18 @@ public class StockWidgetIntentService extends IntentService {
 
 
 
-        String symbol = "GOOG";
+
         int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
         Bundle extras = intent.getExtras();
         if (extras != null) {
             mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
         }
 
-        Log.d(String.valueOf(mAppWidgetId), "- AppWidgetID" );
+        String symbol = "GOOG";
+        Log.d(String.valueOf(appWidgetId), "- AppWidgetID" );
         SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
 
-        //TODO implement a way to get it to work with prefix key
+
         symbol = prefs.getString("stockSymbol", "No name defined");
 
 
@@ -99,31 +115,71 @@ public class StockWidgetIntentService extends IntentService {
             data.close();
             return;
         }
+        */
 
-            //extract the data from the cursor
-            String stockSymbol = data.getString(INDEX_STOCK_SYMBOL);
-            Log.d(stockSymbol, "symbol:");
-            float stockPrice = data.getFloat(INDEX_STOCK_PRICE);
-            float stockChange = data.getFloat(INDEX_STOCK_ABSOLUTE_CHANGE);
-            data.close();
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
+                StockWidgetProvider.class));
 
-            //TODO 1. add setting option for the stock
-            //TODO 2. fix the +/- symbol in front of the stock.
-            //TODO 3. fix the layout - 1 pane would probably be good
+
+
+
+
+
 
             //loop for each widget linking the views
             for (int appWidgetId : appWidgetIds) {
+
+                //get stock from prefs
+                SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+                String prefKey = PREF_PREFIX_KEY + appWidgetId;
+                Log.d(prefKey, "PREFKEY IN FOR");
+                String symbol = prefs.getString(prefKey, "No name defined");
+                Log.d(symbol, "SYMBOL IN FOR");
+
+                //get the data
+
+                Uri mUri = Contract.Quote.makeUriForStock(symbol);
+                Cursor data = getContentResolver().query(mUri, STOCK_HISTORY_PROJECTION, null, null, null);
+
+                if (data == null) return;
+                if (!data.moveToFirst()) {
+                    data.close();
+                    return;
+                }
+
+                //extract the data from the cursor
+                String stockSymbol = data.getString(INDEX_STOCK_SYMBOL);
+                Log.d(stockSymbol, "symbol:");
+                float stockPrice = data.getFloat(INDEX_STOCK_PRICE);
+                float stockChange = data.getFloat(INDEX_STOCK_ABSOLUTE_CHANGE);
+                data.close();
+
+                Log.d(String.valueOf(appWidgetId), "appWidgetId within for loop");
                 int layoutId = R.layout.widget_stock_summary;
                 RemoteViews views = new RemoteViews(getPackageName(), layoutId);
                 views.setTextViewText(R.id.widget_symbol, String.valueOf(stockSymbol));
-                views.setTextViewText(R.id.widget_price, "$" + String.valueOf(stockPrice));
-                views.setTextViewText(R.id.widget_change, "$" + String.valueOf(stockChange));
+                views.setContentDescription(R.id.widget_symbol, getString(R.string.stock_history_detail_btns)+stockSymbol);
+                //views.setContentDescription(R.id.widget_price, getString(R.string.stock_history_detail_btns)+stockSymbol);
+                //views.setContentDescription(R.id.widget_change, getString(R.string.stock_history_detail_btns)+stockSymbol);
+                if (stockChange > 0) {
+                    views.setInt(R.id.widget_change, "setBackgroundResource", R.drawable.percent_change_pill_green);
+                } else {
+                    views.setInt(R.id.widget_change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+                }
+
+                //format our doubles nicely
+                String change = dollarFormatWithPlus.format(stockChange);
+                String dollarPrice = dollarFormat.format(stockPrice);
+                views.setTextViewText(R.id.widget_price, dollarPrice);
+                views.setTextViewText(R.id.widget_change, change);
 
                 Intent launchIntent = new Intent(this, MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
                 views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
                 appWidgetManager.updateAppWidget(appWidgetId, views);
+
 
             }
 
